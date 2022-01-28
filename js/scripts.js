7452126bd91a0dbdc6a7b39a25c2342d6b5db91e
JeremyDelorme <jeremydelorme23.7@gmail.com>
2022-01-28 11:23:32 -0500

    added a for loop with if...else condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-alert('Hello world');
+//Creating list of three pokemons with three different attributes
 
-//Added square brackets to 'fire' type.
 let PokemonList= [
   {
     name: 'Bulbasaur',
@@ -21,9 +20,13 @@ let PokemonList= [
 
 console.log(PokemonList);
 
+//Creating a for loop that iterates over each item in pokemonList
+
 for (let i = 0; i < PokemonList.length; i++) {
+  //adding 'if' condition to pokemons over 3.5 of height
   if(PokemonList[i].height > 3.5) {
     document.write('<p>' +  PokemonList[i].name + ( ', height: ' )+ PokemonList[i].height + ( " (Wow, That\'s big !!)") + '<p>');
+  //adding 'else' condition to all other pokemons
  } else {
    document.write('<p>' +  PokemonList[i].name + ( ', height: ' )+ PokemonList[i].height + ( " (That's not so big)") + '<p>');
  }
