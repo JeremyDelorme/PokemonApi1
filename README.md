787f0438a26b4a419931cd119c2817c4cc215108
JeremyDelorme <jeremydelorme23.7@gmail.com>
2022-02-20 12:09:08 -0600

    Added Performance Elements
    1. Added ESLint and Prettier to Atom
    2. Fine-tuned Styling
    3. Added a README file
    4. Put js and css folders in src folder
    5. Fixed few problems

---
+++ README.md
@@ -0,0 +1,7 @@
+Pokedex APP
+
+This is my first JavaScript Project.
+
+It consists of a Pokedex App which retrieves information from an API to display on a usable and responsive app. For this App, I used HTML, CSS and JS, along with external libraries like Bootstrap and JQuery.
+
+Simply click on any of the 150 Pokemons, and details about them will show.
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,7 +5,7 @@ let pokemonRepository = (function() {
   //Assigned an Array to the variable 'pokemonList'
   let pokemonList = [];
   //Added API URL
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   //Created a function to call the pokemonList array
   function getAll() {
@@ -15,11 +15,11 @@ let pokemonRepository = (function() {
   //Created a Function to ADD a Pokemon
   function add(newPokemon) {
     //Validated whether all parameters are objects
-    if (typeof newPokemon === "object") {
+    if (typeof newPokemon === 'object') {
       pokemonList.push(newPokemon);
       //If they aren't objects
     } else {
-      console.log("pokemon is not correct");
+      console.log('pokemon is not correct');
     }
   }
 
@@ -35,9 +35,15 @@ let pokemonRepository = (function() {
     //Created the variable 'openModalButton' from the 'button' class
     let openModalButton = document.createElement('button');
     //Defined Text inside the Button
-    openModalButton.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    openModalButton.innerText =
+      pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     //Added classes to the openModalButton variable
-    openModalButton.classList.add("button-class", "btn", "btn-primary", "btn-sm");
+    openModalButton.classList.add(
+      'button-class',
+      'btn',
+      'btn-primary',
+      'btn-sm'
+    );
 
     //Added the data toggle to trigger the modal
     openModalButton.setAttribute('data-toggle', 'modal');
@@ -52,67 +58,76 @@ let pokemonRepository = (function() {
     //Added an Event Listener for when user clicks on each Pokemon
     openModalButton.addEventListener('click', function() {
       showDetails(pokemon);
-    })
+    });
   }
 
   //Created a function LOAD the details and PRINT them onto screen
   function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
+    loadDetails(pokemon).then(function() {
       showModal(pokemon);
     });
   }
 
   //Created a function to LOAD the LIST of pokemon List from apiUrl
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url,
-        };
-        add(pokemon);
+    return fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
       });
-    }).catch(function (e) {
-      console.error(e);
-    });
   }
 
   //Created a function to LOAD the DETAILS of the pokemon List from apiUrl
   function loadDetails(item) {
     //Assigned the variable 'url' to the details of each item
     let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      //Added the details to the item
-      item.imgUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.weight = details.weight;
-      item.types = [];
-       for (let i = 0; i < details.types.length; i++) {
-         let typesDetails = details.types[i].type.name;
-         item.types.push(typesDetails[0].toUpperCase() + typesDetails.substring(1));
-       }
-       item.abilities = [];
-       for (let i = 0; i < details.abilities.length; i++) {
-         let abilitiesDetails = details.abilities[i].ability.name;
-         item.abilities.push(abilitiesDetails[0].toUpperCase() + abilitiesDetails.substring(1));
-       }
-    }).catch(function (e) {
-      console.error(e);
-    });
+    return fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(details) {
+        //Added the details to the item
+        item.imgUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.weight = details.weight;
+        item.types = [];
+        for (let i = 0; i < details.types.length; i++) {
+          let typesDetails = details.types[i].type.name;
+          item.types.push(
+            typesDetails[0].toUpperCase() + typesDetails.substring(1)
+          );
+        }
+        item.abilities = [];
+        for (let i = 0; i < details.abilities.length; i++) {
+          let abilitiesDetails = details.abilities[i].ability.name;
+          item.abilities.push(
+            abilitiesDetails[0].toUpperCase() + abilitiesDetails.substring(1)
+          );
+        }
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
   }
 
+  //<-- END OF POKEMON REPOSITORY SCRIPT
 
-//<-- END OF POKEMON REPOSITORY SCRIPT
-
-//BEGGINNING OF MODAL SCRIPT -->
+  //BEGGINNING OF MODAL SCRIPT -->
 
   //Create a Function to SHOW the Modal with the POKEMON DETAILS
   function showModal(pokemon) {
-    let modalHeader = $('.modal-header');
+    //Assigned 'modalTitle' variable to modal-title class
     let modalTitle = $('.modal-title');
     let modalBody = $('.modal-body');
 
@@ -120,16 +135,21 @@ let pokemonRepository = (function() {
     modalTitle.empty();
     modalBody.empty();
 
-
     //Modal Header
-    let titleElement = $('<h1>' + pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) + '</h1>');
+    let titleElement = $(
+      '<h1>' +
+        pokemon.name.charAt(0).toUpperCase() +
+        pokemon.name.slice(1) +
+        '</h1>'
+    );
     let heightElement = $('<p>' + 'Height: ' + pokemon.height + ' cm' + '</p>');
-    let weightElement = $('<p>' + 'Weight: ' + pokemon.weight + ' pounds' + '</p>');
+    let weightElement = $(
+      '<p>' + 'Weight: ' + pokemon.weight + ' pounds' + '</p>'
+    );
     let typesElement = $('<p>' + 'Types: ' + pokemon.types + '</p>');
 
     let imageElement = $('<img class="modal-img" style="width:50%">');
-    imageElement.attr("src", pokemon.imgUrl);
-
+    imageElement.attr('src', pokemon.imgUrl);
 
     //Appending titleElement to the modalTitle
     modalTitle.append(titleElement);
@@ -142,7 +162,7 @@ let pokemonRepository = (function() {
 
   //<-- END OF MODAL SCRIPT
 
-//Returned the six(6) Functions inside the IIFE
+  //Returned the six(6) Functions inside the IIFE
   return {
     add: add,
     getAll: getAll,
@@ -156,10 +176,9 @@ let pokemonRepository = (function() {
 //<-- END OF IIFE FOR POKEMON REPOSITORY
 
 //Loaded list of Pokemons
-pokemonRepository.loadList().then(function () {
-
+pokemonRepository.loadList().then(function() {
   //Printed a list of buttons FOR EACH pokemon in the repository
-  pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
